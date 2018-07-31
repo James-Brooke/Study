3e77ee19235f5f94d12c89b9e1bba198cb9f8b60
James-Brooke <jamesbrooke290@gmail.com>
2018-07-31 12:14:30 +0100

    moving funcs to module

--- PyTorch/Darwin.py
+++ PyTorch/Darwin.py
@@ -1,5 +1,3 @@
-%matplotlib inline
-
 import torch
 import random
 import copy
@@ -19,6 +17,41 @@ from torchvision import datasets, transforms
 from operator import itemgetter
 from tqdm import tqdm, tnrange, tqdm_notebook
 
+
+def random_value(space):
+    """Returns random value from space."""
+    
+    val = None
+    
+    if 'func' in space: #randomise optimiser or activation function
+        val = random.sample(space['func'], 1)[0] 
+    
+    elif isinstance(space['lb'], int): #randomise number of units or layers
+        val = random.randint(space['lb'], space['ub'])
+    
+    else: #randomise percentages, i.e. dropout rates or weight decay
+        val = random.random() * (space['ub'] - space['lb']) + space['lb']
+    
+    return val
+
+
+def randomize_network(layer_space, net_space): 
+    """Returns a randomised neural network"""
+    net = {}
+    
+    for key in net_space.keys():
+        net[key] = random_value(net_space[key])
+        
+    layers = []
+    
+    for i in range(net['nb_layers']):
+        layer = {}
+        for key in layer_space.keys():
+            layer[key] = random_value(layer_space[key])
+        layers.append(layer)
+        net['layers'] = layers
+        
+    return net
 
 
 
