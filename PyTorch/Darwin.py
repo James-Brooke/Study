011cb0c315dc58c50262a4300eff855dd9f9c671
James-Brooke <jamesbrooke290@gmail.com>
2018-07-31 12:17:16 +0100

    netfrombuildinfo

--- PyTorch/Darwin.py
+++ PyTorch/Darwin.py
@@ -53,5 +53,121 @@ def randomize_network(layer_space, net_space):
         
     return net
 
+def mutate_net(nnet, layer_space, net_space):
+    """Mutates a network hyperparameters as defined 
+    in layer_space and net_space
+    """
+    net = copy.deepcopy(nnet)
+    
+    
+    # mutate optimizer
+    for k in ['lr', 'weight_decay', 'optimizer']:
+        if random.random() < net_space[k]['mutate']:
+            net[k] = random_value(net_space[k])
+    
+    
+    # mutate layers
+    for layer in net['layers']:
+        for k in layer_space.keys():
+            if random.random() < layer_space[k]['mutate']:
+                layer[k] = random_value(layer_space[k])
+                
+                
+    # mutate number of layers -- 50% add 50% remove
+    if random.random() < net_space['nb_layers']['mutate']:
+        if net['nb_layers'] <= net_space['nb_layers']['ub']:
+            if random.random()< 0.5 and \
+            net['nb_layers'] < net_space['nb_layers']['ub']:
+                layer = {}
+                for key in layer_space.keys():
+                    layer[key] = random_value(layer_space[key])
+                net['layers'].append(layer)      
+            else:
+                if net['nb_layers'] > 1:
+                    net['layers'].pop()
+
+                
+            # value & id update
+            net['nb_layers'] = len(net['layers'])         
+            
+    return net
+
+class Flatten(nn.Module):
+    """Flattens input to vector size (batchsize, 1)
+    (for use in NetFromBuildInfo).
+    """
+
+    def __init__(self):
+        super(Flatten, self).__init__()
+
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
+
+class NetFromBuildInfo(nn.Module):
+    def __init__(self, build_info):
+        super(NetFromBuildInfo, self).__init__()
+        
+        self.activation_dict = {
+            'tanh': nn.Tanh(),
+            'relu': nn.ReLU(),
+            'sigmoid': nn.Sigmoid(),
+            'elu': nn.ELU()
+            }
+
+        #NETWORK DEFINITION
+        
+        previous_units = 28 * 28 #MNIST shape
+        
+        self.model = nn.Sequential()
+        self.model.add_module('flatten', Flatten())
+         
+        for i, layer_info in enumerate(build_info['layers']):
+            i = str(i)
+            
+            self.model.add_module(
+                'fc_' + i,
+                nn.Linear(previous_units, layer_info['nb_units'])
+                )
+            
+            previous_units = layer_info['nb_units']
+            
+            self.model.add_module(
+                'dropout_' + i,
+                nn.Dropout(p=layer_info['dropout_rate'])
+                )
+            if layer_info['activation'] == 'linear':
+                continue #linear activation is identity function
+            self.model.add_module(
+                layer_info['activation']+ i,
+                self.activation_dict[layer_info['activation']])
+
+        self.model.add_module(
+            'logits',
+            nn.Linear(previous_units, 10) #10 MNIST classes
+            )
+        
+        
+        ##OPTIMIZER
+
+        self.opt_args = {#'params': self.model.parameters(),
+                 'weight_decay': build_info['weight_decay'],
+                 'lr': build_info['lr']
+                 }
+        
+        self.optimizer_dict = {
+            'adam': optim.Adam(self.model.parameters(),**self.opt_args),
+            'rmsprop': optim.RMSprop(self.model.parameters(),**self.opt_args),
+            'adadelta':optim.Adadelta(self.model.parameters(),**self.opt_args),
+            'sgd': optim.SGD(self.model.parameters(), **self.opt_args, momentum=0.9) #momentum to train faster
+            }
+
+        self.optimizer = self.optimizer_dict[build_info['optimizer']]
+        
+        
+    def forward(self, x):
+        x = self.model(x)
+        return F.log_softmax(x, dim=1)
+
 
 
